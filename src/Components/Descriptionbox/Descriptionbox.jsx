3e7cf7f8985d7527196ac8eff675f93a8bca9e85
akashsharma1706 <akashsharma200010@gmail.com>
2024-05-01 13:42:44 +0530

    ecommerce app

---
+++ src/Components/Descriptionbox/Descriptionbox.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Descriptionbox.css'
+
+const Descriptionbox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews(122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora molestiae nulla, dolorem maiores doloremque odit veniam porro corporis nam sequi vero perferendis suscipit veritatis mollitia explicabo ratione adipisci modi iusto!</p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi a perspiciatis harum neque illo reprehenderit repellat officiis vitae temporibus assumenda, tempore exercitationem. Beatae nihil, error dolorem ab eaque veniam numquam!
+      </p>
+      </div>
+    </div>
+  )
+}
+
+export default Descriptionbox
